3521b968dad59efa8ec73e94106686b563ad6f7e
phenanth <ewe_dom@yahoo.co.jp>
2019-02-20 13:48:04 +0800

    UPDATE: Mataining.

--- AFF/app/docs/config.py
+++ AFF/app/docs/config.py
@@ -15,7 +15,7 @@ The global veriables are defined here.
 # directory_path = os.path.abspath(os.path.join(os.sep, 'Users', 'Administrator', 'Documents', 'AFF_Downloader'))
 
 # Use this if you're running server as a docker container(python setup.py docker)
-directory_path = os.path.abspath(os.path.join(os.sep, "home", "skhr-0324", "project", "data", "AFF_Downloader"))
+directory_path = os.path.abspath(os.path.join(os.sep, "root", "Documents", "AFF_Downloader"))
 
 # The file path of README
 README = 'README.md'
@@ -25,7 +25,7 @@ README = 'README.md'
 # Downloader
 
 # Cookie
-cookies = dict(cookies_are="__cfduid=db6a38f42ec2cc7353f1be4a623bd00481548502011; _ga=GA1.2.228529505.1548502020; fitracking_6=no; rtk_gdpr_a=0; rtk_gdpr_c=JP; _pubcid=fee0db94-cdcf-42d1-830b-7f2e56164767; __gads=ID=e5a2fad591bc45d1:T=1548502036:S=ALNI_MYFeUVjfZJ4TNTLjJ8brvLbrvq8YQ; asianfanficspref=%7B%22siteversion%22%3A1%2C%22theme%22%3A%22blue%22%7D; csrf_aff_cookie=b52606222a943fd800067bcd1cd3e9a7; cmVsYXRl=93e817aa5f; _gid=GA1.2.308696800.1550480746; affsession=vl405akf286thrkp48ja0fvhe5frkr1c; YWN0aXZl=1")
+cookies = dict(cookies_are="__cfduid=db6a38f42ec2cc7353f1be4a623bd00481548502011; _ga=GA1.2.228529505.1548502020; fitracking_6=no; rtk_gdpr_a=0; rtk_gdpr_c=JP; _pubcid=fee0db94-cdcf-42d1-830b-7f2e56164767; __gads=ID=e5a2fad591bc45d1:T=1548502036:S=ALNI_MYFeUVjfZJ4TNTLjJ8brvLbrvq8YQ; asianfanficspref=%7B%22siteversion%22%3A1%2C%22theme%22%3A%22blue%22%7D; csrf_aff_cookie=b52606222a943fd800067bcd1cd3e9a7; cmVsYXRl=93e817aa5f; _gid=GA1.2.308696800.1550480746; YWN0aXZl=1; fi_utm=direct%7Cdirect%7C%7C%7C%7C; affsession=2etpk16uvbeisqv91glivjbrl5frk8a7; _gat=1")
 
 # The common part of url path, the dashboard estat web api
 url_path = "https://www.asianfanfics.com/story/view/"
@@ -50,7 +50,7 @@ url_trans = "http://api.fanyi.baidu.com/api/trans/vip/translate"
 LINE_END = "\n"
 SENTENCE_END = "\n\n"
 
-partition = 5000
+partition = 2000
 ENTER = "\n"
 
 fromLang = 'en'
