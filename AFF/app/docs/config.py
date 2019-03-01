a23b7fe6dfb4abcd7db9b6e09aaa51dab9c5ebe6
phenanth <ewe_dom@yahoo.co.jp>
2019-03-01 11:59:47 +0800

    Maintaing.

--- AFF/app/docs/config.py
+++ AFF/app/docs/config.py
@@ -27,7 +27,7 @@ README = 'README.md'
 # Downloader
 
 # Cookie
-cookies = dict(cookies_are="__cfduid=db6a38f42ec2cc7353f1be4a623bd00481548502011; _ga=GA1.2.228529505.1548502020; fitracking_6=no; rtk_gdpr_a=0; rtk_gdpr_c=JP; _pubcid=fee0db94-cdcf-42d1-830b-7f2e56164767; __gads=ID=e5a2fad591bc45d1:T=1548502036:S=ALNI_MYFeUVjfZJ4TNTLjJ8brvLbrvq8YQ; asianfanficspref=%7B%22siteversion%22%3A1%2C%22theme%22%3A%22blue%22%7D; csrf_aff_cookie=b52606222a943fd800067bcd1cd3e9a7; cmVsYXRl=93e817aa5f; _gid=GA1.2.308696800.1550480746; YWN0aXZl=1; fi_utm=direct%7Cdirect%7C%7C%7C%7C; affsession=2etpk16uvbeisqv91glivjbrl5frk8a7; _gat=1")
+cookies = dict(cookies_are="__cfduid=db6a38f42ec2cc7353f1be4a623bd00481548502011; _ga=GA1.2.228529505.1548502020; rtk_gdpr_a=0; rtk_gdpr_c=JP; _pubcid=fee0db94-cdcf-42d1-830b-7f2e56164767; __gads=ID=e5a2fad591bc45d1:T=1548502036:S=ALNI_MYFeUVjfZJ4TNTLjJ8brvLbrvq8YQ; csrf_aff_cookie=b52606222a943fd800067bcd1cd3e9a7; cmVsYXRl=93e817aa5f; YWN0aXZl=1; asianfanficspref=%7B%22siteversion%22%3A1%2C%22theme%22%3A%22blackwhite%22%7D; _gid=GA1.2.824081122.1551360913; fitracking_6=yes; fiTrackingDomainParams=%7B%22host%22%3A%22tracking1.firstimpression.io%22%2C%22type%22%3A%22full%22%7D; affsession=s8mrg4k6ss0le959l19fmmta9te11p6q; dmxRegion=false; _pk_ses.6574.3985=*; fi_utm=direct%7Cdirect%7C%7C%7C%7C; _gat=1; asianfanficsurl=%2Fstory%2Fview%2F1032346%2F3; _pk_id.6574.3985=1b54e8d0f1f6e015.1551360924.2.1551411938.1551411734.")
 
 # The common part of url path, the dashboard estat web api
 url_path = "https://www.asianfanfics.com/story/view/"
@@ -59,10 +59,10 @@ fromLang = 'en'
 toLang = 'zh'
 
 # APP ID
-# appid = '20190212000265967'
+appid = '20190212000265967'
 # Sub APP ID
-appid = '20190221000269592'
-# 密钥
-# secretKey = 'xbpxFLPvxh9MLMikS6sw'
+# appid = '20190221000269592'
+# Secret Key
+secretKey = 'xbpxFLPvxh9MLMikS6sw'
 # Sub Secret Key
-secretKey = 'EMzCxdhMSlN812I7M1GC'
+# secretKey = 'EMzCxdhMSlN812I7M1GC'
